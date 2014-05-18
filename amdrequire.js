d03883e54a25388f3adf0c946cf66fa739a34a24
Javi Marquez <javi@arqex.com>
2014-05-18 11:26:52 +0200

    Absolute urls and node require style working inside define for AMD require

--- amdrequire.js
+++ amdrequire.js
@@ -1,112 +1,190 @@
 var Module = require('module'),
 	path = require('path'),
+
+	// Are we on windows?
 	win = path.sep === '\\',
-	noderequire = function(dep, module){
+
+	// Node require
+	noderequire = function(dep, thisModule){
 		//Call _load directly as we can't set this object for require.
-		return Module._load(dep, module);
+		return Module._load(dep, thisModule);
 	},
+
+	// Named paths loaded from the config as 'name': 'path'
 	paths = {},
 
-	//Win also understand '/' as directory separator
+	// Stack of paths used to load relative dependencies
+	pathStack = [],
+
+	// Stack of modules to let define access to its module
+	defineModuleStack = [],
+
+	// Stack of defines to know if we are inside a define method.
+	defineStack = [],
+
+	// The baseUrl setting of require.js translated to path. Overrided by the config method.
 	basePath = path.dirname(process.mainModule.filename) + path.sep;
+	publicPath = basePath + 'public' + path.sep;
 
 	requireStack = [],
-	amdCache = {},
+	defineValues = {},
 
-	amdrequire = function(deps, callback){
+	// AMD require
+	amdrequire = function(deps, callback, thisModule){
 		var dependencies = [];
-		deps.forEach(function(dep){
-			console.log('dep ' + dep);
-			var depPath = resolveAmdPath(dep);
-			if(typeof Module._cache[dep] == 'undefined'){
-				var filepath = paths[dep] ||
-				console.log('inside amdrequire ' + dep);
-				requireStack.push(dep);
+		defineModuleStack.push(thisModule);
 
-				noderequire(depPath);
-				console.log('DEPPATH ' + depPath + '.js');
-				//console.log(Module._cache);
-				console.log(Module._cache[depPath + '.js']);
-				console.log(amdCache[dep]);
-				Module._cache[depPath + '.js'].exports = amdCache[dep];
-				console.log(Module._cache[depPath + '.js']);
+		deps.forEach(function(depName){
+			//Get absolute path
+			var depPath = resolveAmdPath(depName, thisModule),
+				depExport = {}
+			;
+
+			//Normalize, no .js extension
+			if(path.extname(depPath) == '.js')
+				depPath = depPath.substring(0, depPath.length - 3);
+
+			if(typeof Module._cache[depPath + '.js'] == 'undefined'){
+
+				// Push the path as the id of this require. Define will use to store its output.
+				requireStack.push(depPath);
+
+				// Push the current path in case we needed to resolve more relative paths
+				pathStack.push(path.dirname(depPath));
+				noderequire(depPath, thisModule);
+				pathStack.pop();
+
+				depExport = defineValues[depPath];
+				Module._cache[depPath + '.js'].exports = depExport;
+
+				delete defineValues[depPath];
+
+				requireStack.pop();
 			}
-			//console.log(Module._cache[dep]);
-			dependencies.push(amdCache[dep]);
+			else {
+				depExport = Module._cache[depPath + '.js'].exports;
+			}
+
+			//Add to the current dependencies
+			dependencies.push(depExport);
 		});
+		defineModuleStack.pop();
+
+		// Use the dependencies
 		if(callback)
-			callback.apply(this, dependencies);
+			return callback.apply(this, dependencies);
 	},
 
+	// Hybrid require ;)
 	require = function(deps, callback){
+		var depExports = {},
+			thisModule = this && this.exports ? this : module
+		;
+
+		// Push the current path in case we needed to resolve more relative paths
+		pathStack.push(path.dirname(thisModule.parent.filename));
+
+		// AMD require
 		if(Object.prototype.toString.call( deps ) === '[object Array]'){
-			console.log('amdrequire ' + deps);
-			return amdrequire(deps, callback);
+			depExports = amdrequire(deps, callback, thisModule);
+		}
+		else { // Node require
+			//If we are inside a define we can use require('string') notation to get amdrequire modules
+			if(defineStack.length && paths[deps])
+				deps = paths[deps];
+			depExports = noderequire(deps, thisModule);
 		}
 
-		var thisModule = this && this.exports ? this : module;
-		return noderequire(deps, thisModule);
+		//Get out of this path
+		pathStack.pop();
+
+		return depExports;
 	},
+
+	// AMD module define
 	define = function(deps, callback){
 		var depName = requireStack[requireStack.length - 1],
 			exp = {}
 		;
 
-		console.log('Using define');
-		if(typeof deps == 'function')
-			exp = callback.call(require, module.exports, module);
-		else if(Object.prototype.toString.call( deps ) === '[object Array]' && (typeof callback == 'function')){
-			exp = callback();
+		defineStack.push(depName);
+
+		// AMD define
+		if(Object.prototype.toString.call( deps ) === '[object Array]' && (typeof callback == 'function')){
+			//Require all the dependencies
+			exp = amdrequire(deps, callback, defineModuleStack[defineModuleStack.length - 1]);
 		}
+		// Define a module directly as an object
 		else if(deps !== null && typeof deps === 'object'){
-			console.log('Object require ' + deps);
 			exp = deps;
 		}
 
-		console.log('Define ' + depName + ': ');
-		console.log(exp);
-		amdCache[depName] = exp;
-	},
-	config = function(settings){
-		if(settings.paths)
-			paths = settings.paths;
-		if(settings.baseUrl)
-			basePath = settings.baseUrl;
+		// Get out the define
+		defineStack.pop();
+
+		// Leave the value in teh defineValues for amdrequire
+		defineValues[depName] = exp;
 	},
 
-	resolveAmdPath = function(request){
-		var start = request.substring(0, 2), filepath;
+	// Config function to reuse require.js named modules
+	config = function(settings){
+		if(settings.basePath)
+			basePath = settings.basePath;
+		if(settings.publicPath)
+			publicPath = settings.publicPath;
+
+		if(settings.paths){
+			for (var depName in settings.paths){
+				var depPath = settings.paths[depName];
+				if(win)
+					depPath = depPath.replace(/\//g, '\\');
+
+				//Normalize, no .js extension
+				if(path.extname(depPath) == '.js')
+					depPath = depPath.substring(0, depPath.length - 3);
+
+				//Store absolute paths
+				paths[depName] = path.resolve(basePath, depPath);
+			}
+		}
+	},
+
+	// Resolve amd require urls to absolute paths
+	resolveAmdPath = function(request, thisModule){
+		var start = request.substring(0, 2),
+			filepath
+		;
+		if(request[0] == '/'){
+			// Absolute url
+			filepath = win ? request.substring(1).replace(/\//g, '\\') : request.substring(1);
+			return path.resolve(publicPath, filepath);
+		}
 		if (start !== './' && start !== '..' && start !== '.\\'){
 			if(paths[request]){
 				//Named require
-				filepath = paths[request];
-				if(win)
-					filepath.replace(/\//g, '\\');
-				console.log('Named require ' +  filepath);
-				return resolveAmdPath(filepath);
+				return paths[request];
 			}
 
-			//Absolute path
 			filepath = win ? request.replace(/\//g, '\\') : request;
-			console.log('Resolving absolute path ' + filepath);
-			return filepath;
+			//Either it is a windows absolute path or relative to the basePath
+			var lookupPaths = [path.dirname(filepath), path.dirname(path.resolve(basePath, filepath))],
+				requestName = path.basename(request, '.js')
+			;
+			// Let node look up the path for us
+			return Module._findPath(requestName, lookupPaths);
 		}
 
+		// Relative path to the current one
 		filepath = win ? request.replace(/\//g, '\\') : request;
-		console.log('Resolving ' + request + ' to ' + path.resolve(basePath, filepath));
-		return path.resolve(basePath, filepath);
+		return path.resolve(pathStack[pathStack.length - 1], filepath);
 	}
 ;
 
-console.log("SEPARATOR: " + path.sep);
-console.log("SEPARATOR: " + (path.sep === '\\'));
-console.log(basePath);
-
-//Make AMD require available.
+// Make AMD require available.
 require.config = config;
 Module.prototype.require = require;
-global.define = define;
 
-//console.log(process.mainModule);
+// Let define be used in all modules
+global.define = define;
 
 module.exports = require;
